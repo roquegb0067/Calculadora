944efadc850a33b66ff7c58d876be7bae917e24d
roquegb0067 <roque.gbrl14@gmail.com>
2026-09-10 14:50:06 -0300

    ajustes na descompactação do json para variáveis para montagem dos cards

--- videos/script.js
+++ videos/script.js
@@ -16,45 +16,21 @@ function montar_cards(classe_video, titulo_video, id_video, id) {
 }
 async function Receber_json() {
     try {
-    const resposta = await fetch('http://127.0.0.1:3000/api/videos');
-    if (!resposta.ok) throw new Error(`HTTP Error: ${resposta.status}`);
+    const RetornoVideos = await fetch('http://127.0.0.1:3000/api/videos');
+    if (!RetornoVideos.ok) {
+  throw new Error(`HTTP error! Status: ${RetornoVideos.status}`);
 
-    const listaDeVideos = await resposta.json();
-
+    const resposta = await RetornoVideos.json();, 
+    const { classe_video, titulo_video, id_video, id } = resposta;
     // Percorre cada vídeo retornado do Rust
     listaDeVideos.forEach(video => {
-      montar_cards(video.classe_video, video.titulo_video, video.id_video, video.id);
+      montar_cards(classe_video, titulo_video, id_video, id);
     });
 
+  return { classe_video, titulo_video, id_video, id };
   } catch (error) {
     console.error('Erro ao buscar vídeos:', error);
   }
 }
 
 Receber_json();
-    
-    
-    
-/*    
-    
-    
-    // Como é GET, basta passar a URL
-    const RetornoVideos = await fetch('http://127.0.0.1:3000/api/videos');
-    
-    if (!RetornoVideos.ok) {
-      throw new Error(`HTTP error! Status: ${RetornoVideos.status}`);
-    }
-    
-    const resposta = await RetornoVideos.json();
-    const { classe_video, titulo_video, id_video, id } = resposta;
-    listaDeVideos.forEach(video => {
-      montar_cards(video.classe_video, video.titulo_video, video.id_video, video.id);
-    });
-    return { classe_video, titulo_video, id_video, id };
-  } catch (error) {
-    console.error('Failed to fetch data:', error);
-  }
-}
-
-// Executa automaticamente para carregar os vídeos assim que entrar na página
-Receber_json();*/
